44df6c336f8f06109ed2a81863517ef7a622338b
kimberly burton <kj.burton03@gmail.com>
2023-09-14 15:30:19 -0400

    started working on project pages

--- src/components/nashMurals/nashMurals.js
+++ src/components/nashMurals/nashMurals.js
@@ -1,5 +1,16 @@
 export const NashMurals = () => {
     return <>
         <h1> Nash Murals </h1>
+        <a href="https://oyster-app-vi6os.ondigitalocean.app" > nash murals</a>
+        <h2> Code </h2>
+        {/* <a href="https://github.com/kjburton03/responsibly_irresponsible" target="_blank" className="code__link" > front end </a>
+        <a href="https://github.com/kjburton03/responsibly_irresponsibly_server" target="_blank" className="code__link" > back end </a> */}
+
+
+        <h2> Tech Used </h2>
+
+        <h2> Wire Frame </h2>
+
+        <h2> DbDiagram </h2>
     </>
 }
