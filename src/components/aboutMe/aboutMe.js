dea1e36a0fea3377927a737b4602ff483d518da3
kimberly burton <kj.burton03@gmail.com>
2023-09-17 16:28:26 -0400

    added tech icons

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -9,7 +9,6 @@ export const AboutMe = () => {
         <h1>Kimberly Burton</h1>
         <h4>Atlanta, Georgia </h4>
 
-        <h4> about me </h4>
         <h4> depending on the day you can find me ...</h4>
             <ul>
                 <li> Bartending in Atlanta </li>
@@ -21,6 +20,10 @@ export const AboutMe = () => {
                 <li> Learning about tech at the nearest coffee shop with a large mug of green tea </li>
                 <li> Goodwill shopping to refurnish in my dad's garage </li>
             </ul>
+        <h4> about me </h4>
+            <p> 
+            spent the last decade bartending from fine dining to 
+            </p>
         <h4> about my coding journey </h4>
         <p>
             I started coding in 2005 making the best backgrounds for my MySpace and Xanga accounts. 
--- src/components/skillz/skills.js
+++ src/components/skillz/skills.js
@@ -1,4 +1,15 @@
 import "./skills.css"
+import html5 from "../../assets/html5.png"
+import css3 from "../../assets/css3.png"
+import django from "../../assets/django.png"
+import github from "../../assets/github.png"
+import javascript from "../../assets/javascript.png"
+import postman from "../../assets/postman.png"
+import python from "../../assets/python.png"
+import react from "../../assets/react.png"
+import tailwind from "../../assets/tailwind.png"
+
+
 export const Skills = () => {
     return <>
         <h1>what can she dooooo</h1>
@@ -6,8 +17,20 @@ export const Skills = () => {
 
         <h3 className="skill__titles">Utilities</h3>
         <div className="tech__stack__container">
-        <img className="tech__stack" src="https://i.imgur.com/0psTasJ.png" alt="github" />
-            <img className="tech__stack" src="https://i.imgur.com/dv3bDci.png" alt="hmmm" />
+
+        <img src={css3} className="tech__stack" />
+        <img src={html5} className="tech__stack" />
+        <img src={javascript} className="tech__stack" />
+        <img src={react} className="tech__stack" />
+        <img src={tailwind} className="tech__stack" />
+        
+        <img src={python} className="tech__stack" />
+        <img src={django} className="tech__stack" />
+
+        <img src={postman} className="tech__stack" />
+
+        <img src={github} className="tech__stack" />
+
         </div>
     </> 
 
